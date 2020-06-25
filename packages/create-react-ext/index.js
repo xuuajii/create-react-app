9ce87a928be68d8be53809f74a932e7f392b5cdd
paolo <paoloandreaderegibus@yahoo.it>
2020-06-25 18:16:04 +0200

    minor changes

--- packages/create-react-ext/index.js
+++ packages/create-react-ext/index.js
@@ -1,5 +1,11 @@
 const execSync = require('child_process').execSync;
+const commander = require('commander');
+
+packageJson = require('./package.json');
+const program = new commander.Command(packageJson.name).parse(process.argv);
+console.log(program.args[0]);
+
 const extCreated = execSync(
-  'npx create-react-app my-ext --template extension',
+  `npx create-react-app ${program.args[0]} --template extension`,
   { stdio: 'inherit' }
 );
