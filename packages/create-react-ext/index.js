9fa012ea5526cbb08b85616fc41899e412863523
paolo <paoloandreaderegibus@yahoo.it>
2020-06-26 10:44:16 +0200

    set up new mode

--- packages/create-react-ext/index.js
+++ packages/create-react-ext/index.js
@@ -2,34 +2,23 @@ const execSync = require('child_process').execSync;
 const commander = require('commander');
 
 packageJson = require('./package.json');
-const program = new commander.Command(packageJson.name).parse(process.argv);
-console.log(program.args[0]);
+const program = new commander.Command(packageJson.name);
+//program.option('-d --dev', 'is in dev mode')
+program.parse(process.argv); //.parseOptions(process.argv)
+// console.log(program.dev)
+// const instruction = {
+//   command:'npx create-react-app',
+//   appName: program.args[0],
+//   template:  '--template extension'
+//   scripts:program.args[1]==='dev'
+// }
 
 try {
   const extCreated = execSync(
-    `npx create-react-app ${program.args[0]} --template extension`,
+    `npx create-react-app ${program.args[0]} --template extension --scripts-version react-ext-scripts@0.0.0`,
     { stdio: 'inherit' }
   );
 } catch (err) {
   console.log('app creation failed');
-  process.exit(1);
-}
-console.clear();
-try {
-  const extCreated = execSync(`npm uninstall react-scripts`, {
-    stdio: 'inherit',
-  });
-} catch (err) {
-  console.log('failed to uninstall react-scripts');
-  process.exit(1);
-}
-
-try {
-  const extCreated = execSync(`npm install react-ext-scripts`, {
-    stdio: 'inherit',
-  });
-  console.log('initializtion completed');
-} catch (err) {
-  console.log('failed to install react-ext-scripts');
   process.exit(1);
 }
