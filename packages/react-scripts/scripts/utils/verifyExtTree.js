ea3a8671853dbda760dc7e1b91b6496ffad81586
paolo <paoloandreaderegibus@yahoo.it>
2020-06-29 16:58:55 +0200

    added check on template ext file content

--- packages/react-scripts/scripts/utils/verifyExtTree.js
+++ packages/react-scripts/scripts/utils/verifyExtTree.js
@@ -1,6 +1,7 @@
 const paths = require('../../config/paths');
 const fs = require('fs-extra');
 const chalk = require('react-dev-utils/chalk');
+
 const path = require('path');
 const verifyExtTree = () => {
   const appName = require(paths.appPackageJson).name;
@@ -20,9 +21,28 @@ const verifyExtTree = () => {
             ' please create the file and rerun the script'
         )
       );
+      if (fe === 'js') {
+        checkExtFile(filePath);
+      }
       process.exit(1);
     }
   });
+  const checkExtFile = filePath => {
+    const templateDef = fs
+      .readFileSync(
+        '../../node_modules/cra-template-extension/template/public/react-ext-scripts.js'
+      )
+      .toString()
+      .trim();
+    const fileContent = fs.readFileSync(filePath).toString().trim();
+    if (templateDef !== fileContent) {
+      console.log(
+        chalk.yellow(
+          'Template js file has been changed. This is not supported and can cause unexpected behaviour.'
+        )
+      );
+    }
+  };
 };
 
 module.exports = verifyExtTree;
